cafb6d849bcae9777ff91506929d7171768f5a38
MatyaSina <matyassinaadugna@gmail.com>
2025-09-25 16:36:04 +0300

    Django app, part 5

--- polls/models.py
+++ polls/models.py
@@ -11,9 +11,15 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    # def was_published_recently(self):
+    #     # Was this published within the last 24 hours?
+    #     return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    # bug fixed
     def was_published_recently(self):
         # Was this published within the last 24 hours?
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
