8928015f05586db193749b020a8570974e2304dd
MatyaSina <matyassinaadugna@gmail.com>
2025-09-25 13:21:58 +0300

    Django app, part 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -104,7 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# Default: 'America/Chicago'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Addis_Ababa'
 
 USE_I18N = True
 
--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from django.utils import timezone
+import datetime
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        # Was this published within the last 24 hours?
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) # Each Choice is associated with a Question
+    choice_text = models.CharField(max_length=200)
+    vote = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
